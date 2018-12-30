89b411e8355feed5fe31e70c302526fa581f86ee
satyendra gupta <sat8ndra@gmail.com>
2018-12-30 23:47:56 +0530

    Added authentication token for get todos

--- server/server.js
+++ server/server.js
@@ -18,11 +18,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   //console.log(req.body);
   var todo = new Todo({
     text: req.body.text,
-    completed: req.body.completed
+    _creator: req.user._id
+    //completed: req.body.completed
   });
   todo.save().then((doc) => {
     res.send(doc);
@@ -31,8 +32,10 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
